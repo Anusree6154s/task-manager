e97a083b0e2ea9f5fe8f36b773b9e0994d0aa979
Anusree <anilkumaranusree113@gmail.com>
2024-10-17 20:30:54 +0530

    initial commit - add frontend + backend

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/task.routes");
+
+
+const app = express();
+const PORT = 8082;
+const DB_URI = process.env.MONGO_URI;
+// const DB_URI = "mongodb://localhost:27017/task-manager";
+
+mongoose
+    .connect(DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connected!"))
+    .catch((error) => console.log("Error in connecting DB", error));
+
+app.use(cors());
+app.use(express.json());
+app.use("/tasks", router);
+
+app.listen(PORT, () => {
+    console.log(`Backend listening on Port ${PORT}!`);
+});
